f138b102d3ab4f0ab69fe9a7b0d06271a78fe680
whai <2294278735@qq.com>
2015-03-11 17:38:10 +0800

    2015-3-11 17:38

--- test/a.cpp
+++ test/a.cpp
@@ -1,21 +1,75 @@
 #include<iostream>
 #include<cstdio>
 using namespace std;
-int dp[100][100];
-int main(){
-	dp[1][0]=1;
-	for(int i=2;i<10;++i){
-		int sum=0;
-		for(int j=0;j<=i;++j){
-			sum+=dp[i-1][j];
-			dp[i][j]=sum;
-		}
+#define LL __int64
+const int N=1e6+5;
+const int MOD=2015;
+struct Matrix
+{
+    LL a[3][3];	//
+    int n,m;
+    Matrix(int _n=0,int _m=0,LL val=0)
+    {
+        n=_n; m=_m;
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+                a[i][j]=(i==j?val:0);
+    }
+    void print()
+    {
+        for(int i=0;i<n;i++,puts(""))
+            for(int j=0;j<m;j++)
+                cout<<a[i][j]<<' ';
+        puts("");
+    }
+    Matrix operator *(Matrix tmp)
+    {
+        Matrix ret(n,tmp.m);
+        for(int i=0;i<n;i++)
+            for(int j=0;j<tmp.m;j++)
+                for(int k=0;k<m;k++)
+                    ret.a[i][j]=(ret.a[i][j]+a[i][k]*tmp.a[k][j])%MOD;	//
+        return ret;
+    }
+    Matrix operator ^(LL b)
+    {
+        Matrix ret(n,m,1),base=(*this);
+        while(b)
+        {
+            if(b&1) ret=ret*base;
+            b>>=1;
+            base=base*base;
+        }
+        return ret;
+    }
+};
+LL a[N],b[N];
+void init(){
+	a[0]=1;
+	for(int i=1;i<N;++i){
+		a[i]=a[i-1]*i%MOD;
+		if(i==1) b[i]=0;
+		else b[i-1]=a[i-2]*(i-1)*(i-1)%MOD;
 	}
-	for(int i=1;i<10;++i){
-		for(int j=0;j<i;++j){
-			cout<<dp[i][j]<<' ';
-		}
-		cout<<endl;
+	//for(int i=0;i<6;++i){
+	//	cout<<a[i]<<' '<<b[i]<<endl;
+	//}
+	//cout<<endl;
+}
+int main(){
+	init();
+	int T;
+	scanf("%d",&T);
+	while(T--){
+		int n,k;
+		scanf("%d%d",&n,&k);
+		Matrix m0=Matrix(1,2),p=Matrix(2,2);
+		m0.a[0][0]=a[k]; m0.a[0][1]=b[k];
+		p.a[0][0]=k+1; p.a[1][0]=1; p.a[1][1]=k;
+		p=p^(n-k-1);
+		m0=m0*p;
+		//m0.print();
+		printf("%I64d\n",m0.a[0][0]);
 	}
 	return 0;
 }
--- test/bl.cpp
+++ test/bl.cpp
@@ -1,18 +1,24 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
+bool ok(string a){
+	for(int i=0;i<a.length()-1;++i){
+		if(a[i]>a[i+1]) return false;
+	}
+	return true;
+}
 int gao(string a){
 	int ret=0;
-	for(int i=0;i<a.length();++i){
-		for(int j=i+1;j<a.length();++j){
-			if(a[i]>a[j]) ret++;
-		}
+	while(!ok(a)){
+		for(int i=0;i<a.length()-1;++i)
+			if(a[i]>a[i+1]) swap(a[i],a[i+1]);
+		++ret;
 	}
 	return ret;
 }
 int o[20];
 int main(){
-	string a="1234";
+	string a="12345678";
 	o[0]=1;
 	while(next_permutation(a.begin(),a.end())){
 		int cnt=gao(a);
